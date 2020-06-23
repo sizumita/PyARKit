a84d00c815a7b1b2103b91ffdbf23de0bec26d06
Sumito Izumita <sumito@izumita.com>
2020-06-23 09:54:38 +0900

    [Add] Materialを作成 [Add] Lightを作成 [Add] Colorのclassを作成

--- PyARKit/material.py
+++ PyARKit/material.py
@@ -1,2 +1,51 @@
+from objc_util import ObjCClass, ObjCInstance, CGRect, CGPoint, CGSize, load_framework
+from color import Color
+load_framework('SceneKit')
+load_framework('ARKit')
+
+SCNScene = ObjCClass('SCNScene')
+SCNView = ObjCClass('SCNView')
+SCNNode = ObjCClass('SCNNode')
+SCNCamera = ObjCClass('SCNCamera')
+SCNMaterial = ObjCClass('SCNMaterial')
+SCNBox = ObjCClass('SCNBox')
+UIColor = ObjCClass('UIColor')
+SCNLight = ObjCClass('SCNLight')
+
+ARWorldTrackingConfiguration = ObjCClass('ARWorldTrackingConfiguration')
+ARSCNView = ObjCClass('ARSCNView')
+ARSession = ObjCClass('ARSession')
+
+
 class Material:
-    pass
+    def __init__(self, obj, position=None):
+        obj.material().setColor_(UIColor.cyanColor().CGColor())
+        self.obj = obj
+        self.position = position or (0, 0, 0)
+        self.node = SCNNode.nodeWithGeometry_(self.obj)
+        if True:
+            self.set_position()
+
+    def set_position(self):
+        self.node.setPosition_(self.position)
+
+    def set_color(self, color):
+        if not isinstance(color, Color):
+            self.obj.setColor_(color)
+        self.obj.setColor_(color.get())
+    
+    @classmethod
+    def Box(cls, width=0, height=0, length=0, chamferRadius=0, position=None):
+        box_obj=SCNBox.boxWithWidth_height_length_chamferRadius_(width, height, length, chamferRadius)
+        return cls(box_obj, position)
+
+
+class Light(Material):
+    def __init__(self, position=None, light_type='omni'):
+        self.obj = SCNLight.light()
+        self.position = position or (0, 0, 0)
+        self.obj.setType_(light_type)
+        self.node = SCNNode.node()
+        self.node.setLight_(self.obj)
+        self.set_color(UIColor.greenColor().CGColor())
+
--- PyARKit/world.py
+++ PyARKit/world.py
@@ -1,5 +1,5 @@
 import ui
-from .material import Material
+from material import Material, Light
 from objc_util import ObjCClass, ObjCInstance, CGRect, CGPoint, CGSize, load_framework
 
 load_framework('SceneKit')
@@ -19,39 +19,39 @@ ARSCNView = ObjCClass('ARSCNView')
 ARSession = ObjCClass('ARSession')
 
 
-class World:
+class World(ui.View):
     def __init__(self, frame=None,):
         self.frame = frame or (0, 0, 100, 100)
         self.width = self.frame[2]
         self.height = self.frame[3]
-        self.ui_view = ui.View()
         flex_width, flex_height = (1 << 1), (1 << 4)
-        self.instance = ObjCInstance(self.ui_view)
+        self.instance = ObjCInstance(self)
         self.rect = CGRect(CGPoint(0, 0), CGSize(self.width, self.height))
         self.scn_view = ARSCNView.alloc().initWithFrame_options_(self.rect, None).autorelease()
         self.scn_view.setAutoresizingMask_(flex_width|flex_height)
 
         self.scene = SCNScene.scene()
-        self.ui_view.scene = self.scene
-        self.ui_view.showsStatistics = 1
-
+        self.scn_view.showsStatistics = 1
+        self.scn_view.scene = self.scene
+        
         self.ar_configuration = ARWorldTrackingConfiguration.new()
         self.ar_session = ARSession.new()
-        self.ar_session.delegate = self.ui_view
+        self.ar_session.delegate = self
         self.ar_session.runWithConfiguration_(self.ar_configuration)
         self.scn_view.setSession_(self.ar_session)
         
         self.instance.addSubview_(self.scn_view)
-
+        
     def add_childmaterial(self, material):
         if not isinstance(material, Material):
             raise ValueError('It is not material instance')
         self.scene.rootNode().addChildNode_(material.node)
 
-    def present(self):
-        self.ui_view.present()
-
 
 if __name__ == '__main__':
     w = World()
+    m = Material.Box(height=0.1, width=0.1, length=0.1, chamferRadius=0.1, position=(0, 0, -0.2))
+    l = Light(position=(0, 20, -0.2))
+    w.add_childmaterial(m)
+    w.add_childmaterial(l)
     w.present()
